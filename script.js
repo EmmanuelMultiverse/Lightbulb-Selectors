b088b3fc6159fee49d545012ffdf7d4c1eb1de3a
Emmanuel Luis <ejluis2001@gmail.com>
2025-04-30 14:12:19 -0500

    Finished task using querySelectorAll

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Write your code here
+let lightbulbs = document.querySelectorAll(".item.lightbulb");
+let subtitle = document.querySelector(".subtitle");
+let count = 0;
+
+lightbulbs.forEach((item) => item.addEventListener("click", () => {
+    ++count;
+    item.classList.toggle("active");
+    subtitle.innerHTML = `You've clicked the lights ${count} times`;
+}))
